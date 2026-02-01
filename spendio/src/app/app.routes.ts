5229befd79e11f7dbe7c7bbe511e061fd65e327e
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-01 18:51:40 +0530

    Integrate navbar into expense tracker page

--- spendio/src/app/app.routes.ts
+++ spendio/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ExpenseTrackerComponent } from './pages/expense-tracker/expense-tracker.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'expenseTracker',
+        component: ExpenseTrackerComponent
     }
 ];
