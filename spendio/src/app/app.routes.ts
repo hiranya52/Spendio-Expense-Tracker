59b24a4aa67ef6d74b16ac4fc9d4d6479927387c
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-01 21:59:01 +0530

    Add contact us page

--- spendio/src/app/app.routes.ts
+++ spendio/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ExpenseTrackerComponent } from './pages/expense-tracker/expense-tracker.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'expenseTracker',
         component: ExpenseTrackerComponent
+    },
+    {
+      path: 'contactUs',
+      component: ContactUsComponent
     }
 ];
