030803032601569dea405f494ed2548ccdac4353
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-01 21:35:16 +0530

    Add navigation from expense tracker to home

--- spendio/src/app/pages/expense-tracker/expense-tracker.component.ts
+++ spendio/src/app/pages/expense-tracker/expense-tracker.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-expense-tracker',
-  imports: [FooterComponent],
+  imports: [FooterComponent, RouterLink],
   templateUrl: './expense-tracker.component.html',
   styleUrl: './expense-tracker.component.css'
 })
