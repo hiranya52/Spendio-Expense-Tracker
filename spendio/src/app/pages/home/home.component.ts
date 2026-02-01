f8b7f4c040dddd69dd9174cad3fc25cc3530aef9
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-01 09:56:05 +0530

    Implement and style features section

--- spendio/src/app/pages/home/home.component.ts
+++ spendio/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { HeroComponent } from "../../components/hero/hero.component";
+import { FeaturesComponent } from "../../components/highlights/highlights.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, HeroComponent],
+  imports: [NavBarComponent, HeroComponent, FeaturesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
