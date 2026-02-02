33eb3913651b7102657f448cde94e7d99392ab2e
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-02 16:34:58 +0530

    Add footer to contact us page

--- spendio/src/app/pages/contact-us/contact-us.component.ts
+++ spendio/src/app/pages/contact-us/contact-us.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, FooterComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
