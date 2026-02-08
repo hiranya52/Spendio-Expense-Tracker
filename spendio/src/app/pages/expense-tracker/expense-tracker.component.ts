54fce5e33196dbd6723831ee57252ea11ee73ea2
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-08 16:14:54 +0530

    Include CommonModule and FormsModule for template binding

--- spendio/src/app/pages/expense-tracker/expense-tracker.component.ts
+++ spendio/src/app/pages/expense-tracker/expense-tracker.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-expense-tracker',
-  imports: [FooterComponent, RouterLink],
+  imports: [FooterComponent, RouterLink, CommonModule, FormsModule],
   templateUrl: './expense-tracker.component.html',
   styleUrl: './expense-tracker.component.css'
 })
