0c2bcff16afbe038f9b1755ee3f55b825c572244
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-09 15:52:53 +0530

    Extract contact faq section into faq-section component

---
+++ spendio/src/app/components/faq-section/faq-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-section',
+  imports: [],
+  templateUrl: './faq-section.component.html',
+  styleUrl: './faq-section.component.css'
+})
+export class FaqSectionComponent {
+
+}
--- spendio/src/app/pages/contact-us/contact-us.component.ts
+++ spendio/src/app/pages/contact-us/contact-us.component.ts
@@ -4,10 +4,11 @@ import { FooterComponent } from '../../components/footer/footer.component';
 import { ContactInfoComponent } from "../../components/contact-info/contact-info.component";
 import { ContactHeroComponent } from "../../components/contact-hero/contact-hero.component";
 import { ContactFormComponent } from "../../components/contact-form/contact-form.component";
+import { FaqSectionComponent } from "../../components/faq-section/faq-section.component";
 
 @Component({
   selector: 'app-contact-us',
-  imports: [NavBarComponent, FooterComponent, ContactInfoComponent, ContactHeroComponent, ContactFormComponent],
+  imports: [NavBarComponent, FooterComponent, ContactInfoComponent, ContactHeroComponent, ContactFormComponent, FaqSectionComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
