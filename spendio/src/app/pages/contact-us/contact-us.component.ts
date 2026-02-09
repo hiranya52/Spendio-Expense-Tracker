0317c4ce99b622fbb0c00dcb3b1f8af30aea7d1e
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-09 15:31:15 +0530

    Extract Contact Info section into a separate component

--- spendio/src/app/pages/contact-us/contact-us.component.ts
+++ spendio/src/app/pages/contact-us/contact-us.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ContactInfoComponent } from "../../components/contact-info/contact-info.component";
+import { ContactHeroComponent } from "../../components/contact-hero/contact-hero.component";
 
 @Component({
   selector: 'app-contact-us',
-  imports: [NavBarComponent, FooterComponent],
+  imports: [NavBarComponent, FooterComponent, ContactInfoComponent, ContactHeroComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
