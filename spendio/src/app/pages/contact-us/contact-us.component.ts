bff61c93f256a8e6e712a452a40ee1fb0f978d70
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-09 15:38:27 +0530

    Split contact form section into a separate component

--- spendio/src/app/pages/contact-us/contact-us.component.ts
+++ spendio/src/app/pages/contact-us/contact-us.component.ts
@@ -3,10 +3,11 @@ import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ContactInfoComponent } from "../../components/contact-info/contact-info.component";
 import { ContactHeroComponent } from "../../components/contact-hero/contact-hero.component";
+import { ContactFormComponent } from "../../components/contact-form/contact-form.component";
 
 @Component({
   selector: 'app-contact-us',
-  imports: [NavBarComponent, FooterComponent, ContactInfoComponent, ContactHeroComponent],
+  imports: [NavBarComponent, FooterComponent, ContactInfoComponent, ContactHeroComponent, ContactFormComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
