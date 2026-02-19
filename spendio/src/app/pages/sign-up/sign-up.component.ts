44d6d38f0d91e571c60c1ae6ee98c331df663c9d
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-19 16:36:45 +0530

    Implement navigation between login and sign up pages

--- spendio/src/app/pages/sign-up/sign-up.component.ts
+++ spendio/src/app/pages/sign-up/sign-up.component.ts
@@ -1,8 +1,9 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
