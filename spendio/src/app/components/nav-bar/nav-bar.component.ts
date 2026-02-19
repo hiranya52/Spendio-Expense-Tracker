17b1b8738310a28c70d063cfde8bba574e909359
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-19 11:56:10 +0530

    create responsive navbar with mobile menu

--- spendio/src/app/components/nav-bar/nav-bar.component.ts
+++ spendio/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
 }
