40fabd9afbde82c523e05be5417c4ef0a8d7436a
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-19 18:59:18 +0530

    Configure router and HTTP client providers

--- spendio/src/app/pages/home/home.component.ts
+++ spendio/src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { HeroComponent } from "../../components/hero/hero.component";
 import { FeaturesComponent } from "../../components/highlights/highlights.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { SignUpComponent } from "../sign-up/sign-up.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, HeroComponent, FeaturesComponent, FooterComponent],
+  imports: [NavBarComponent, HeroComponent, FeaturesComponent, FooterComponent, SignUpComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- spendio/src/app/pages/sign-up/sign-up.component.ts
+++ spendio/src/app/pages/sign-up/sign-up.component.ts
@@ -1,14 +1,14 @@
 import { UserService } from './../../service/user/user.service';
-import { user } from './../../../model/user.model';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, RouterModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
+
 })
 export class SignUpComponent {
 
@@ -27,7 +27,6 @@ export class SignUpComponent {
     })
 
   }
-
 
   onSubmit(){
 
