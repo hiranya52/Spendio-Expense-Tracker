9711e1ceeee49d55b2b62b71739b9167a4f39cdd
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-19 16:33:16 +0530

    Introduce and design sign up page

--- spendio/src/app/pages/home/home.component.ts
+++ spendio/src/app/pages/home/home.component.ts
@@ -5,10 +5,11 @@ import { FeaturesComponent } from "../../components/highlights/highlights.compon
 import { FooterComponent } from "../../components/footer/footer.component";
 import { ExpenseTrackerComponent } from "../expense-tracker/expense-tracker.component";
 import { LogInComponent } from "../log-in/log-in.component";
+import { SignUpComponent } from "../sign-up/sign-up.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, HeroComponent, FeaturesComponent, FooterComponent, ExpenseTrackerComponent, LogInComponent,],
+  imports: [NavBarComponent, HeroComponent, FeaturesComponent, FooterComponent, ExpenseTrackerComponent, LogInComponent, SignUpComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- spendio/src/app/pages/log-in/log-in.component.ts
+++ spendio/src/app/pages/log-in/log-in.component.ts
@@ -1,8 +1,9 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-log-in',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
 })
