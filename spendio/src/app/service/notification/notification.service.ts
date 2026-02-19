291fa33987c37f775711b5b75b65b5737434c14f
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-19 21:21:51 +0530

    Add manual close method to notification toast

--- spendio/src/app/service/notification/notification.service.ts
+++ spendio/src/app/service/notification/notification.service.ts
@@ -11,12 +11,29 @@ export class NotificationService {
   private toastSource = new BehaviorSubject<string | null>(null);
   toast$ = this.toastSource.asObservable();
 
+   private timeoutId: any; // store setTimeout so we can cancel it
+
+
   show(message: string) {
+    // clear any previous timeout
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+
     this.toastSource.next(message);
 
-    setTimeout(() => {
+    // auto hide after 2.5s
+    this.timeoutId = setTimeout(() => {
       this.toastSource.next(null);
     }, 2500);
+  }
+
+  // manually close toast
+  close() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+    this.toastSource.next(null);
   }
 
 }
