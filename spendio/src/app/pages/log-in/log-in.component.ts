d0d3a45f2246e4b96b5d267b57f311aa3af297b6
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-19 20:39:24 +0530

    Implement login form with reactive form handling

--- spendio/src/app/pages/log-in/log-in.component.ts
+++ spendio/src/app/pages/log-in/log-in.component.ts
@@ -1,12 +1,38 @@
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-log-in',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
 })
 export class LogInComponent {
+
+  loginForm: FormGroup;
+
+  email!:string;
+  password!:string;
+
+  constructor(private fb: FormBuilder) {
+
+    this.loginForm = this.fb.group({
+      email: [''],
+      password: ['']
+    });
+
+  }
+
+  onLogin() {
+
+    this.email = this.loginForm.value.email;
+    this.password = this.loginForm.value.password;
+
+    
+
+
+  }
+
 
 }
