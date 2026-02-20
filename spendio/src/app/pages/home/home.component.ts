ced38f895abc28a29f714a40d6df94851bf9b275
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-20 14:30:52 +0530

    Add modal form for adding new expenses

--- spendio/src/app/pages/home/home.component.ts
+++ spendio/src/app/pages/home/home.component.ts
@@ -4,10 +4,11 @@ import { HeroComponent } from "../../components/hero/hero.component";
 import { FeaturesComponent } from "../../components/highlights/highlights.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { SignUpComponent } from "../sign-up/sign-up.component";
+import { AddExpenseComponent } from "../../components/add-expense/add-expense.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, HeroComponent, FeaturesComponent, FooterComponent, SignUpComponent],
+  imports: [NavBarComponent, HeroComponent, FeaturesComponent, FooterComponent, SignUpComponent, AddExpenseComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
